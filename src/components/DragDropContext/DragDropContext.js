d470a91d87405b5156675dcba4afd364f5b12faa
Dinesh Yalla <dineshyalla@gmail.com>
2020-06-14 21:15:09 -0700

    Din dragdropcontext (#4)
    * created file dragdropcontext
    
    * added one line

--- src/components/DragDropContext/DragDropContext.js
+++ src/components/DragDropContext/DragDropContext.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Layout } from './styled';
 
 const DragDropContext = () => {
-  return(
-  <Layout> 
-    <h3>This is DragDropContext</h3>
-  </Layout>
-  )
+  return (
+    <Layout>
+      <h3>This is DragDropContext</h3>
+    </Layout>
+  );
 };
 
 export default DragDropContext;
