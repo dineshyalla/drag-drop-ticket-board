1088f13ddfe51ee9a184412129ccc593e402691e
Dinesh <dineshyalla@gmail.com>
2020-06-14 20:51:01 -0700

    ticket file rename

--- src/components/ticket.js
+++ src/components/ticket.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { Layout } from './styled';
 
 const Ticket = () => {
-    return(
-        <li>
-        <Layout draggable="true">
-        this is ticket
-        </Layout>
-        </li>
-    )
-}
+  return (
+    <li>
+      <Layout draggable="true">this is ticket</Layout>
+    </li>
+  );
+};
 
 export default Ticket;
