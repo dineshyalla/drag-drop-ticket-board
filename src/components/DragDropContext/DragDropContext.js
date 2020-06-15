bc7ee87b7a8905d8e482cc9a53e2cc1283d5f6c2
Dinesh Yalla <dineshyalla@gmail.com>
2020-06-14 21:35:19 -0700

    Din dragdropcontext (#5)
    * created file dragdropcontext
    
    * added one line
    
    * created basic columns

--- src/components/DragDropContext/DragDropContext.js
+++ src/components/DragDropContext/DragDropContext.js
@@ -1,11 +1,62 @@
 import React from 'react';
 import { Layout } from './styled';
+import Ticket from '../Ticket/Ticket';
+import uuid from 'react-uuid';
 
 const DragDropContext = () => {
+
+  const columnsFromBackend = {
+    [uuid()]: {
+      name: 'Rewards',
+      items: [
+        'Jeff Bezos',
+        'Bill Gates',
+        'Warren Buffett',
+        'Bernard Arnault',
+        'Carlos Slim Helu',
+        'Amancio Ortega',
+        'Larry Ellison',
+        'Mark Zuckerberg',
+        'Michael Bloomberg',
+        'Larry Page',
+      ],
+    },
+    [uuid()]: {
+      name: 'C1',
+      items: [],
+    },
+    [uuid()]: {
+      name: 'C2',
+      items: [],
+    },
+    [uuid()]: {
+      name: 'C3',
+      items: [],
+    },
+    [uuid()]: {
+      name: 'C4',
+      items: [],
+    },
+  };
+
   return (
-    <Layout>
-      <h3>This is DragDropContext</h3>
-    </Layout>
+    <div style={{ display: 'flex', justifyContent: 'center', height: '100%' }}>
+      {Object.entries(columnsFromBackend).map((column) => {
+        return (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <ul className="draggable-list" id="draggable-list">
+              <Ticket />
+            </ul>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
--- src/components/Ticket/Ticket.js
+++ src/components/Ticket/Ticket.js
@@ -2,10 +2,29 @@ import React from 'react';
 import { Layout } from './styled';
 
 const Ticket = () => {
+  const richestPeople = [
+    'Jeff Bezos',
+    'Bill Gates',
+    'Warren Buffett',
+    'Bernard Arnault',
+    'Carlos Slim Helu',
+    'Amancio Ortega',
+    'Larry Ellison',
+    'Mark Zuckerberg',
+    'Michael Bloomberg',
+    'Larry Page',
+  ];
+
   return (
-    <li>
-      <Layout draggable="true">this is ticket</Layout>
-    </li>
+    <div>
+      {[...richestPeople].map((item, index) => {
+        return (
+          <li style={{ listStyle: 'none' }}>
+            <Layout draggable="true">{item}</Layout>
+          </li>
+        );
+      })}
+    </div>
   );
 };
 
